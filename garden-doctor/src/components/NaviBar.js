c8103358cff2ec7e460712863af2e16d651a6d10
daeyubkang <yub102447@gmail.com>
2023-11-14 09:31:06 +0900

    병합 전 푸시

--- garden-doctor/src/components/NaviBar.js
+++ garden-doctor/src/components/NaviBar.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { LOGOUT } from "../store/isLogin";
+import { LOGIN, LOGOUT } from "../store/isLogin";
+import axios from "axios";
 
 const NaviBar = () => {
   const isLogin = useSelector((state) => state.isLogIn);
-  const username = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const jwtToken = sessionStorage.getItem("token");
+
+    if (jwtToken) {
+      axios
+        .post("http://localhost:8000/sign/verify", { token: jwtToken })
+        .then((response) => {
+          const user = response.data.user.id;
+          dispatch({ type: LOGIN, user: user });
+        })
+        .catch((error) => {
+          dispatch({ type: LOGOUT });
+          console.error("JWT verification error:", error);
+        });
+      setLoading(false);
+    } else {
+      dispatch({ type: LOGOUT });
+      setLoading(false);
+    }
+  }, []);
 
   const navigate = useNavigate();
   const logoButton = () => {
@@ -26,6 +49,8 @@ const NaviBar = () => {
   const signupButton = () => {
     navigate("/signup");
   };
+
+  console.log(isLogin);
   return (
     <div>
       <div style={{ width: "100%", height: "10vh", backgroundColor: "beige" }}>
@@ -33,12 +58,16 @@ const NaviBar = () => {
           로고
         </span>
 
-        {isLogin ? (
+        {loading ? (
+          <div>loading...</div>
+        ) : isLogin ? (
           <button onClick={logoutButton}>로그아웃</button>
         ) : (
-          <button onClick={loginButton}>로그인</button>
+          <>
+            <button onClick={loginButton}>로그인</button>
+            <button onClick={signupButton}>회원가입</button>
+          </>
         )}
-        <button onClick={signupButton}>회원가입</button>
       </div>
     </div>
   );
