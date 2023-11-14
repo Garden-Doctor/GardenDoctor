47b2969cafb72ecec03f9d8201ea6db24a0f66eb
DongHo-Kang <gkqjem981113@gmail.com>
2023-11-14 09:41:40 +0900

    병충해 진단 페이지 만드는중(상단바를 pull하기 위한 커밋)

--- garden-doctor/src/components/imageAI/Dignose.js
+++ garden-doctor/src/components/imageAI/Dignose.js
@@ -7,13 +7,18 @@ const Dignose = () => {
     <div className="dignose-main-container">
       <div className="introduce">
         <img src={Dignose2} />
-        <span>농작물 병충해 판단 서비스란?</span>
+        <span className="title">농작물 병충해 판단 서비스란?</span>
         <br></br>
         <div className="introduceDetails">
           작물 사진을 업로드하면 병을 신속하게 식별하여 농작물 건강을 돕는
           서비스입니다.
         </div>
       </div>
+
+      <select name="selectPlant" className="selectPlant">
+        <option value="">가지</option>
+        <option value="">가지</option>
+      </select>
     </div>
   );
 };
