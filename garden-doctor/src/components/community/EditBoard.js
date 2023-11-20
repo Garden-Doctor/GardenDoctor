25310a0ea84bd46367a8c79ff462a626e40c7e67
daeyubkang <yub102447@gmail.com>
2023-11-20 10:19:34 +0900

    ㄷㅂㅈㄷ

--- garden-doctor/src/components/community/EditBoard.js
+++ garden-doctor/src/components/community/EditBoard.js
@@ -112,7 +112,7 @@ const EditBoard = () => {
               onClick={handleImageUploadClick}
             >
               <div className="writeBoard-imagePreview">
-                {/* 이미지 미리보기 */}
+                {/* 이미지 미리보기11 */}
                 <img src={imagePreviewUrl} alt="Preview" />
                 <span>클릭하여 이미지 추가</span>
               </div>
