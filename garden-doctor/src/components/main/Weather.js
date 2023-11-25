67f724cdb0ef325e2e603b5442a17be2cacb3b6c
jinorable@gmail.com <jinorabel@gmail.com>
2023-11-25 13:33:23 +0900

    weather

--- garden-doctor/src/components/main/Weather.js
+++ garden-doctor/src/components/main/Weather.js
@@ -1,57 +1,95 @@
-// src/components/Weather.js
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Weather = () => {
-  const [location, setLocation] = useState(null);
-  const [weather, setWeather] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [currentDateTime, setCurrentDateTime] = useState(null);
 
-  const getCurrentWeather = async () => {
+  useEffect(() => {
+    // 접속 시 위치 정보 담기
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          // console.log(latitude, longitude);
+          setCurrentLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting current location:", error.message);
+        }
+      );
+    }
+
+    // 현재 날짜와 시간 가져오기
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      setCurrentDateTime(now);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    // 현재 위치가 설정되면 서버에 날씨 정보 요청
+    if (currentLocation) {
+      fetchWeatherData();
+    }
+  }, [currentLocation]);
+
+  const fetchWeatherData = async () => {
     try {
-      if (location) {
-        const response = await axios.get(
-          `/weather?lat=${location.latitude}&lon=${location.longitude}`
-        );
-        setWeather(response.data);
-      }
+      const response = await axios.post("http://localhost:8000/weather", {
+        currentdate: getCurrentDate(),
+        currenttime: getCurrentTime(),
+        inputx: currentLocation.latitude,
+        inputy: currentLocation.longitude,
+      });
+
+      const data = response;
+      console.log("날씨", data);
+      setWeatherData(data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
   };
 
-  useEffect(() => {
-    // 최초 렌더링 후 5초마다 날씨 정보 업데이트
-    const intervalId = setInterval(getCurrentWeather, 5000);
+  const getCurrentDate = () => {
+    if (currentDateTime) {
+      const year = currentDateTime.getFullYear();
+      const month = currentDateTime.getMonth() + 1;
+      const day = currentDateTime.getDate();
 
-    // 컴포넌트가 마운트될 때 사용자의 현재 위치를 가져옴
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      (error) => {
-        console.error("Error getting current position:", error);
-      }
-    );
+      return `${year}${month < 10 ? "0" + month : month}${
+        day < 10 ? "0" + day : day
+      }`;
+    }
 
-    // 컴포넌트가 언마운트될 때 타이머 정리
-    return () => clearInterval(intervalId);
-  }, []); // 빈 배열을 전달하여 최초 한 번만 실행되도록 함
+    return "";
+  };
+
+  // 분 단위 버리고 정시만 담게
+  const getCurrentTime = () => {
+    if (currentDateTime) {
+      const hours = currentDateTime.getHours();
+      return `${hours < 10 ? "0" + hours : hours}00`;
+    }
+    return "";
+  };
 
   return (
-    <div className="weather-container">
-      <h1>현재 날씨</h1>
-      {weather ? (
+    <div>
+      <p>Current Date: {getCurrentDate()}</p>
+      <p>Current Time: {getCurrentTime()}</p>
+      {weatherData && (
         <div>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Condition: {weather.weather[0].description}</p>
-          {/* Add more weather information as needed */}
+          <h2>Weather Information</h2>
+          {/* {weatherData.TMP} */}
+          {/* <p>Temperature: {weatherData.TMP}</p>
+          <p>Precipitation Probability: {weatherData.POP}</p>
+          <p>Precipitation Amount: {weatherData.PCP}</p> */}
+          {/* 기타 필요한 날씨 정보를 표시하는 부분 */}
         </div>
-      ) : (
-        <p>현재 날씨 불러오는 중</p>
       )}
     </div>
   );
