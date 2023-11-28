49a5241c1a2107d8d8562ea4664b510e95c6e1a6
jinorable@gmail.com <jinorabel@gmail.com>
2023-11-28 13:40:59 +0900

    weather 완료

--- garden-doctor/src/components/main/Weather.js
+++ garden-doctor/src/components/main/Weather.js
@@ -5,6 +5,8 @@ const Weather = () => {
   const [currentLocation, setCurrentLocation] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
   const [currentDateTime, setCurrentDateTime] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [sky, setSky] = useState(null);
 
   useEffect(() => {
     // 접속 시 위치 정보 담기
@@ -14,6 +16,7 @@ const Weather = () => {
           const { latitude, longitude } = position.coords;
           // console.log(latitude, longitude);
           setCurrentLocation({ latitude, longitude });
+          console.log("currentLocation", currentLocation);
         },
         (error) => {
           console.error("Error getting current location:", error.message);
@@ -34,8 +37,22 @@ const Weather = () => {
     // 현재 위치가 설정되면 서버에 날씨 정보 요청
     if (currentLocation) {
       fetchWeatherData();
+      fetchLocationData();
     }
   }, [currentLocation]);
+
+  const fetchLocationData = async () => {
+    try {
+      const res = await axios.post("http://localhost:8000/weather/location", {
+        lat: currentLocation.latitude,
+        lng: currentLocation.longitude,
+      });
+      const locationData = res.data;
+      setLocation(locationData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const fetchWeatherData = async () => {
     try {
@@ -46,11 +63,46 @@ const Weather = () => {
         inputy: currentLocation.longitude,
       });
 
-      const data = response;
+      const data = response.data;
       console.log("날씨", data);
+      console.log("날씨", data.SKY);
+
       setWeatherData(data);
+
+      if (data.PTY == 0) {
+        switch (data.SKY) {
+          case "1":
+            setSky("맑음");
+            break;
+          case "3":
+            setSky("구름많음");
+            break;
+          case "4":
+            setSky("흐림");
+            break;
+        }
+      } else {
+        setSky("비");
+      }
+
+      console.log("", sky);
     } catch (error) {
       console.error("Error fetching weather data:", error);
+    }
+  };
+
+  const getWeatherImage = () => {
+    switch (sky) {
+      case "맑음":
+        return "imgs/sunny.svg";
+      case "구름많음":
+        return "imgs/구름많음.svg";
+      case "흐림":
+        return "imgs/cloudy.svg";
+      case "비":
+        return "imgs/rainy.svg";
+      default:
+        return ""; // 실제 파일 경로로 바꾸세요
     }
   };
 
@@ -68,6 +120,25 @@ const Weather = () => {
     return "";
   };
 
+  const viewedCurrentDate = () => {
+    if (currentDateTime) {
+      const year = currentDateTime.getFullYear();
+      const month = currentDateTime.getMonth() + 1;
+      const day = currentDateTime.getDate();
+
+      // 0부터 6까지의 값을 가지며, 일요일부터 토요일까지 순서대로입니다.
+      const dayOfWeek = currentDateTime.getDay();
+
+      // dayOfWeek를 사용하여 실제 요일을 얻습니다.
+      const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+      const todayOfWeek = daysOfWeek[dayOfWeek];
+
+      return `${year}.${month}.${day}(${todayOfWeek})`;
+    }
+
+    return "";
+  };
+
   // 분 단위 버리고 정시만 담게
   const getCurrentTime = () => {
     if (currentDateTime) {
@@ -78,17 +149,28 @@ const Weather = () => {
   };
 
   return (
-    <div>
-      <p>Current Date: {getCurrentDate()}</p>
-      <p>Current Time: {getCurrentTime()}</p>
+    <div className="weather-container">
       {weatherData && (
-        <div>
-          <h2>Weather Information</h2>
-          {/* {weatherData.TMP} */}
-          {/* <p>Temperature: {weatherData.TMP}</p>
-          <p>Precipitation Probability: {weatherData.POP}</p>
-          <p>Precipitation Amount: {weatherData.PCP}</p> */}
-          {/* 기타 필요한 날씨 정보를 표시하는 부분 */}
+        <div className="weather-container-inner">
+          <div className="weather_left">
+            <div>
+              <img src="imgs/location.svg" />
+              {location}
+            </div>
+            <span>{viewedCurrentDate()}</span>
+          </div>
+          <div className="weather_right">
+            <span className="weather_right_left">
+              <img src={getWeatherImage()} alt={`날씨: ${sky}`} />
+            </span>
+            <div className="weather_right_right">
+              <span className="currentTemp">{weatherData.TMP}°C</span>
+              <span className="todayTemp">
+                <span className="red">{weatherData.TMX}°</span>/
+                <span className="blue">{weatherData.TMN}°</span>
+              </span>
+            </div>
+          </div>
         </div>
       )}
     </div>
