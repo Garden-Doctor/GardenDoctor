f7d8ee69b9f5fb426e3656c10b72c20495305027
daeyubkang <yub102447@gmail.com>
2023-12-06 11:11:45 +0900

    오류 수정

--- garden-doctor-server/models/index.js
+++ garden-doctor-server/models/index.js
@@ -26,6 +26,16 @@ db.Comment = require("./Comment")(sequelize);
 //모델
 db.Like = require("./Like")(sequelize);
 
+//모델
+db.PlantType = require("./PlantType")(sequelize);
+
+//모델
+db.MyPlants = require("./MyPlants")(sequelize);
+
+//사용자와 식물 관계 설정(다대다)
+db.MyPlants.belongsToMany(db.PlantType, { through: "myPlantsTypes" });
+db.PlantType.belongsToMany(db.MyPlants, { through: "myPlantsTypes" });
+
 // 사용자와 댓글 관계 설정
 db.User.hasMany(db.Board, { foreignKey: "userId" });
 db.Board.belongsTo(db.User, { foreignKey: "userId" });
